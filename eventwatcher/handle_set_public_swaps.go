9c342061af28bd50c3578da4dbbd340aa2e49207
bonedaddy <boned4ddy@protonmail.com>
2021-02-18 18:27:01 -0800

    move channel closures to a defer function

--- eventwatcher/handle_set_public_swaps.go
+++ eventwatcher/handle_set_public_swaps.go
@@ -7,18 +7,17 @@ import (
 )
 
 func (wc *WatchedContract) handleSwapToggles(ctx context.Context) {
+	defer func() {
+		wc.toggleSub.Unsubscribe()
+		close(wc.notifToggleCh)
+		close(wc.toggleCh)
+	}()
 	for {
 		select {
 		case <-ctx.Done():
-			wc.toggleSub.Unsubscribe()
-			close(wc.notifToggleCh)
-			close(wc.toggleCh)
 			return
 		case err := <-wc.toggleSub.Err():
 			wc.logger.Error("toggle subscription error received", zap.Error(err))
-			wc.toggleSub.Unsubscribe()
-			close(wc.notifToggleCh)
-			close(wc.toggleCh)
 			return
 		case evLog := <-wc.toggleCh:
 			wc.logger.Debug("received new event (swap toggle)", zap.Any("event", evLog))
